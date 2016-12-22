17f0c9636a59645890971bca2d78b8bb8dbb9d55
wujiang3 <wujiang3@jd.com>
2016-12-22 16:18:58 +0800

    new

--- src/test/java/test/rpc/ServerStartUp.java
+++ src/test/java/test/rpc/ServerStartUp.java
@@ -4,7 +4,7 @@ import com.bucuoa.west.rpc.remoting.server.*;
 
 public class ServerStartUp {
 	public static void main(String[] args) {
-		Server server = new RPCServerStub();
+//		Server server = new RPCServerStub();
 		// server.register(EchoService.class, EchoServiceImpl.class);
 		// server.start();
 		
